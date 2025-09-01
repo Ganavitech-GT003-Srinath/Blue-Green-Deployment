7393f3d23750f9bcc2b8decb3bea30160e0cf52f
Ganavitech-GT003-Srinath <srinath.r@ganavitech.com>
2025-09-01 15:27:50 +0530

    Changed the start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,12 @@
+# #!/bin/bash
+# set -euo pipefail
+# systemctl enable nextjs.service
+# systemctl restart nextjs.service
+# sleep 3
+
 #!/bin/bash
-set -euo pipefail
-systemctl enable nextjs.service
-systemctl restart nextjs.service
-sleep 3
+cd /srv/nextjs
+npm install --production
+npm run build
+pm2 start npm --name "nextjs" -- start
+pm2 save
