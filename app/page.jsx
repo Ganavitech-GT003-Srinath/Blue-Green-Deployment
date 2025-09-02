549f60095f389ce9089f77ce056b6d257fb013b9
Ganavitech-GT003-Srinath <srinath.r@ganavitech.com>
2025-09-02 17:08:31 +0530

    Version 3

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <main style={{ padding: 20, fontFamily: 'sans-serif' }}>
       <h1>Next.js Blue/Green Demo</h1>
-      <p>Version 2</p>
+      <p>Version 3</p>
     </main>
   );
 }
